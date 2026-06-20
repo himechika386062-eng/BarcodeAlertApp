52a1f0fbab92dddd3d09e19de2e5bd9395909145
Siberian <naoya.yamamoto@ruri.waseda.jp>
2026-06-20 21:00:12 +0900

    Integrate text-only book info from OpenBD API

---
+++ BookResult.cs
@@ -0,0 +1,28 @@
+using System.Text.Json.Serialization;
+
+namespace BarcodeAlertApp
+{
+    public class OpenBdResponse
+    {
+        [JsonPropertyName("summary")]
+        public BookSummary? Summary { get; set; }
+    }
+
+    public class BookSummary
+    {
+        [JsonPropertyName("isbn")]
+        public string Isbn { get; set; } = "";
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; } = "";
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; } = "";
+
+        [JsonPropertyName("publisher")]
+        public string Publisher { get; set; } = "";
+
+        [JsonPropertyName("cover")]
+        public string Cover { get; set; } = ""; // 表紙画像のURL
+    }
+}
