a02f4367193be395adcf9881bc8e6724b91e82ec
Siberian <naoya.yamamoto@ruri.waseda.jp>
2026-06-20 15:10:57 +0900

    Fix missing using directive for SupabaseOptions

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BarcodeAlertApp;
+using Supabase;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
